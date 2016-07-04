0d361ae10bd9324b4b8cdc79be861bad4e920cf2
david <davidyjun@gmail.com>
2016-07-04 22:08:24 +0800

    add result return

--- py/1_decorator_Learn_1.py
+++ py/1_decorator_Learn_1.py
@@ -30,3 +30,23 @@ try_this_shirt(20)
 try_this_shirt(45)
 try_this_skirt('blue')
 try_this_skirt('white')
+
+def salesgirl(method):
+    def serve(*args):
+        print "Salesgirl:Hello, what do you want?", method.__name__
+        return method(*args)
+    return serve
+
+@salesgirl
+def try_this_shirt(size):
+    if size < 35:
+        print "I: %d inches is to small to me" %(size)
+        return False
+    else:
+        print "I:%d inches is just enough" %(size)
+        return True
+
+print '=' * 20
+result = try_this_shirt(38)
+print '=' * 20
+print "Mum:do you want to buy this?", result
